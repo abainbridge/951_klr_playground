89cb3a2af41cd195438ccc2d46bde754c28da185
Andy B <a@b.com>
2026-02-10 13:50:07 +0000

    Fix graph logic - it wasn't possible to represent a graph with a single data point. If you tried, you'd have first_idx == last_idx, which the comment said meant there were no points.

--- simulator/graph.c
+++ simulator/graph.c
@@ -17,7 +17,7 @@ enum { MAX_POINTS = 64 };
 typedef struct {
     graph_point_t points[MAX_POINTS]; // In time order. Oldest first.
     int first_idx;
-    int last_idx;   // if first_idx == last_idx, there are no points.
+    int next_idx;   // if first_idx == next_idx, there are no points.
 } graph_t;
 
 
@@ -29,17 +29,17 @@ void graph_add_point(graph_id_t id, unsigned time, uint8_t val) {
 
     // TODO: check that time is greater than last point.
 
-    int num_points = (g->last_idx - g->first_idx + MAX_POINTS) % MAX_POINTS;
+    int num_points = (g->next_idx - g->first_idx + MAX_POINTS) % MAX_POINTS;
     if (num_points == (MAX_POINTS - 1)) {
         // Already full. Remove oldest point.
         g->first_idx++;
         g->first_idx %= MAX_POINTS;
     }
 
-    g->last_idx++;
-    g->last_idx %= MAX_POINTS;
-    g->points[g->last_idx].time = time;
-    g->points[g->last_idx].val = val;
+    g->points[g->next_idx].time = time;
+    g->points[g->next_idx].val = val;
+    g->next_idx++;
+    g->next_idx %= MAX_POINTS;
 }
 
 void graph_draw(graph_id_t id, unsigned time_now, unsigned time_range_to_display,
@@ -50,7 +50,7 @@ void graph_draw(graph_id_t id, unsigned time_now, unsigned time_range_to_display
     DfColour gray = Colour(220, 220, 220, 255);
     RectFill(g_window->bmp, x, y, w, h, gray);
 
-    if (g->first_idx == g->last_idx)
+    if (g->first_idx == g->next_idx)
         return; // No data to plot
 
     double start_time = (double)time_now - (double)time_range_to_display;
@@ -60,12 +60,13 @@ void graph_draw(graph_id_t id, unsigned time_now, unsigned time_range_to_display
 
     double x2;
     double y2;
-    if (g->points[g->last_idx].time < start_time) {
-        HLine(g_window->bmp, x, y + g->points[g->last_idx].val * scale_y, w, g_colourBlack);
+    int last_idx = (g->next_idx - 1 + MAX_POINTS) % MAX_POINTS;
+    if (g->points[last_idx].time < start_time) {
+        HLine(g_window->bmp, x, y + g->points[last_idx].val * scale_y, w, g_colourBlack);
         return;
     }
 
-    for (int i = g->first_idx; i != g->last_idx; i = (i + 1) % MAX_POINTS) {
+    for (int i = g->first_idx; i != last_idx; i = (i + 1) % MAX_POINTS) {
         graph_point_t *p1 = &g->points[i];
         graph_point_t *p2 = &g->points[(i + 1) % MAX_POINTS];
         if (p2->time < start_time)
